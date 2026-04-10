16920393b9050a11678cbb5ec3fbebcac43a0dce
Roger Green <rhtrading09@gmail.com>
2026-04-10 11:49:46 +0200

    speedtimes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import '@/App.css';
 import RaceAnalyzer from './components/RaceAnalyzer';
-import { fetchKMTidRaceData } from './services/fetchKMTidRaceData';
-import { extractKMTidTimingEntries } from './services/parseKMTidToplist';
 
 function App() {
-  useEffect(() => {
-    const runKMTidTest = async () => {
-      const rawText = await fetchKMTidRaceData('260307');
-      const parsedEntries = rawText ? extractKMTidTimingEntries(rawText) : [];
-
-      console.log('[KMTid test] raw is null:', rawText === null);
-      console.log('[KMTid test] parsed count:', parsedEntries.length);
-      console.log('[KMTid test] first entries:', parsedEntries.slice(0, 3));
-    };
-
-    runKMTidTest();
-  }, []);
-
   return (
     <div className="App dark min-h-screen bg-[#0a0e1a]">
       <RaceAnalyzer />
